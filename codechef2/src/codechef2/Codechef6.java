b306d85a38b6401be2ad52aabc76ca9fb0a99557
Govinda <Govinda@LAPTOP-M8JPN5HQ>
2020-09-06 16:14:00 +0530

    Added sysout as first change

--- codechef2/src/codechef2/Codechef6.java
+++ codechef2/src/codechef2/Codechef6.java
@@ -8,7 +8,7 @@ public class Codechef6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Scanner sc=new Scanner(System.in);
-	     
+	     System.out.println("First change");
 	       int t=sc.nextInt();
 	       for(int i=0;i<t;i++)
 	       {
