80e786c8f8323f78700abc042e79ba71841daef8
Govinda <Govinda@LAPTOP-M8JPN5HQ>
2020-09-06 14:34:41 +0530

    Commiting first Program to local repo

---
+++ codechef2/src/codechef2/Codechef6.java
@@ -0,0 +1,56 @@
+package codechef2;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Codechef6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc=new Scanner(System.in);
+	     
+	       int t=sc.nextInt();
+	       for(int i=0;i<t;i++)
+	       {
+	    	   int[] a=new int[10000000];
+	    	   int l,r,c=0,temp=0;
+	    	   int n=sc.nextInt();
+	    	   for(int j=0;j<n;j++)
+	    	   {
+	    		   l=sc.nextInt();
+	    		   r=sc.nextInt();
+	    		   if(a[l]==0)
+	    		   {
+	    			   temp=0;
+	    			   a[l]=1;
+	    			   for(int k=l+1;k<=r;k++)
+	    				   if(a[k]>0)
+	    				   {
+	    					   temp=1;
+	    					   break;
+	    							   
+	    				   }
+	    			   if(temp==0) {
+	    				   c++;
+	    				   a[r]=1;
+	    			   }
+	    				  
+	    			   else
+	    				   a[l]=0;
+	    		   }
+	    		
+	    			   
+	    	   }
+	    	   
+	    	   int result=n-c;
+	    	   
+	    	   if(c==1)
+	    		   System.out.println("-1");
+	    	   else
+	    		   System.out.println(result);
+	       }
+		
+
+	}
+
+}
